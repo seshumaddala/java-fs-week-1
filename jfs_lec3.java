4e41bb437a298d31069dd781703483917ac8416c
Seshu Maddala <103931218+seshumaddala@users.noreply.github.com>
2024-02-09 09:49:04 +0530

    Add files via upload

---
+++ jfs_lec3.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Lecture3{
+    public static void main(String[] args) {
+        Random r=new Random();
+        int v=r.nextInt(1000);
+        System.out.println(v);
+    }
+}
