595bb7b9d4dd9cb59b5862179ed6e1e64281c3bc
Seshu Maddala <103931218+seshumaddala@users.noreply.github.com>
2024-02-09 14:28:23 +0530

    Add files via upload

---
+++ public class Lecture4_2.java
@@ -0,0 +1,9 @@
+public class Lecture4_1 {
+    public static void main(String[] args) {
+        char ch='A';
+        while(ch<=90){
+            System.out.print(ch);
+            ch++;
+        }
+    }
+}
